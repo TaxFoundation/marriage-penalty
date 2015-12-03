a6776e628231cda651e99b521f0a675b0099b33d
Tom VanAntwerp <vanantwerp@gmail.com>
2015-12-03 14:08:16 -0500

    Change domain

--- js/main.js
+++ js/main.js
@@ -5,23 +5,20 @@ var cellWidth = 2,
       .attr('width', cellWidth * baseDimension).attr('height', cellHeight * baseDimension), // Assumes equal number of rows and columns
     colorize = d3.scale.linear()
       .domain([
+        -0.2,
+        -0.16,
         -0.12,
-        -0.1,
         -0.08,
-        -0.06,
         -0.04,
-        -0.02,
         0,
-        0.02,
         0.04,
-        0.06,
         0.08,
-        0.1,
-        0.12
+        0.12,
+        0.16,
+        0.2
       ])
       .range([
         '#48002a',
-        '#880018',
         '#b71c1c',
         '#f44336',
         '#e57373',
@@ -31,11 +28,10 @@ var cellWidth = 2,
         '#64b5f6',
         '#2196f3',
         '#0d47a1',
-        '#0d276f',
         '#000b33'
       ].reverse());
 
-d3.text("data/kids-0.csv", function(text){ // Available data sets: kids-0.csv, kids-1.csv, kids-2.csv
+d3.text("data/kids-1.csv", function(text){ // Available data sets: kids-0.csv, kids-1.csv, kids-2.csv
   var data = d3.csv.parseRows(text);
   var rowCount = data.length,
       columnCount = data[0].length;
